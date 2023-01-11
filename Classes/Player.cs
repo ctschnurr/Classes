436854d2d642500c97cf269d58da4345c9f4dcb7
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2023-01-11 10:27:42 -0400

    wrote prog!
    looks good

---
+++ Classes/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Player
+    {
+        // fields
+
+        public int x;
+        public int y;
+        public int health;
+        public int shield;
+        public int lives;
+        public string name;
+
+        // methods
+
+        public void TakeDamage(int hp)
+        {
+            health -= hp; // (short form for "health = health - hp")
+        }
+        public Player() // constructor
+        {
+            x = 0;
+            y = 0;
+            health = 50;
+            shield = 0;
+            lives = 3;
+            name = "Newbie";
+
+            Console.WriteLine("Player Created!");
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,11 +8,127 @@ namespace Classes
 {
     internal class Program
     {
+        static Player player;
+        static string input;
+        static int output;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            Console.ReadKey(true);
+            Console.WriteLine("Welcome to Player Creator!");
+            Console.WriteLine("Create a new player? (Y)es / (N)o");
+
+            ConsoleKeyInfo choice = Console.ReadKey(true);
+
+            if (choice.Key == ConsoleKey.N)
+            {
+                return;
+            }
+
+            if (choice.Key == ConsoleKey.Y)
+            {
+                player = new Player();
+            }
+
+            while (choice.Key != ConsoleKey.Q)
+            {
+                Console.WriteLine(" Press any key to continue.");
+                Console.ReadKey(true);
+
+                PlayerDisplay();
+                ShowMenu();
+
+                choice = Console.ReadKey(true);
+                
+                if (choice.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    Console.Write(" Please enter new name: ");
+                    player.name = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine(" Name updated!");
+                }
+
+                if (choice.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    Console.Write(" Please enter new health value: ");
+                    input = Console.ReadLine();
+                    bool isNumber = int.TryParse(input, out output);
+                    if (isNumber == false)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(" That is not a valid value for health.");
+                    }
+                    else
+                    {
+                        player.health = output;
+                        Console.WriteLine(" Health updated!");
+                    }
+                }
+
+                if (choice.Key == ConsoleKey.D3)
+                {
+                    Console.Clear();
+                    Console.Write(" Please enter new lives value: ");
+                    input = Console.ReadLine();
+                    bool isNumber = int.TryParse(input, out output);
+                    if (isNumber == false)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(" That is not a valid value for lives.");
+                    }
+                    else
+                    {
+                        player.lives = output;
+                        Console.WriteLine(" Lives updated!");
+                    }
+                }
+
+                if (choice.Key == ConsoleKey.D4)
+                {
+                    Console.Clear();
+                    Console.Write(" Please enter new shield value: ");
+                    input = Console.ReadLine();
+                    bool isNumber = int.TryParse(input, out output);
+                    if (isNumber == false)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(" That is not a valid value for shield.");
+                    }
+                    else
+                    {
+                        player.shield = output;
+                        Console.WriteLine(" Shield updated!");
+                    }
+                }
+
+            }
         }
+
+        static void PlayerDisplay()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" Name: " + player.name);
+            Console.WriteLine(" Health: " + player.health);
+            Console.WriteLine(" Lives: " + player.lives);
+            Console.WriteLine(" Shield: " + player.shield);
+            Console.WriteLine();
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Please choose from the following options:");
+            Console.WriteLine();
+            Console.WriteLine(" (1) Modify Name");
+            Console.WriteLine(" (2) Modify Health");
+            Console.WriteLine(" (3) Modify Lives");
+            Console.WriteLine(" (4) Modify Shield");
+            Console.WriteLine(" (Q) Quit");
+            Console.WriteLine();
+        }
+
     }
 }
